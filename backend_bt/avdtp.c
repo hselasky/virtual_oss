c11e59df9dbcc3cec3f5532e059dde80dbf6a63f
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2015-10-26 07:58:28 +0000

    virtual_oss: Improve avdtpCheckResponse().

--- backend_bt/avdtp.c
+++ backend_bt/avdtp.c
@@ -101,15 +101,15 @@ avdtpCheckResponse(int recvfd, int *trans, int signalId,
 		if (len == AVDTP_LEN_ERROR)
 			return (buffer[2]);
 		else if ((len % AVDTP_LEN_SUCCESS) == 0 &&
-		    buffer[0] & RESPONSEACCEPT) {
+		    (buffer[0] & RESPONSEACCEPT)) {
 			if (len == AVDTP_LEN_SUCCESS)
 				return (0);
-			else if (datasize && data && len > AVDTP_LEN_SUCCESS) {
-				memcpy(data, buffer + 2, len - 2);
-				*datasize = len - 2;
-
-				return (0);
-			}
+		}
+		if (datasize && data && len > AVDTP_LEN_SUCCESS &&
+		    (buffer[0] & RESPONSEACCEPT)) {
+			memcpy(data, buffer + 2, len - 2);
+			*datasize = len - 2;
+			return (0);
 		}
 	}
 response_invalid:
