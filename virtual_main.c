a5c518657e1f6d238dceb4428ed29782dc9488d2
hselasky <hselasky@4429bdba-5c01-0410-9f4f-ee3375ed255f>
2020-04-29 15:30:28 +0000

    virtual_oss: Implement FIONWRITE.

--- virtual_main.c
+++ virtual_main.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2012-2019 Hans Petter Selasky. All rights reserved.
+ * Copyright (c) 2012-2020 Hans Petter Selasky. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -1218,6 +1218,9 @@ vclient_ioctl_oss(struct cuse_dev *pdev, int fflags,
 		break;
 	case FIONREAD:
 		data.val = vclient_input_delay(pvc);
+		break;
+	case FIONWRITE:
+		data.val = vring_total_write_len(&pvc->tx_ring[1]);
 		break;
 	case FIOASYNC:
 	case SNDCTL_DSP_NONBLOCK:
